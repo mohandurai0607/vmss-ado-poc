25e4dd5b1905cbf467cd91da833f682c8db669b6
Mohan0607 <76526200+Mohan0607@users.noreply.github.com>
2024-10-04 22:33:00 +0530

    Update maven.ps1

--- scriptss-ps/maven.ps1
+++ scriptss-ps/maven.ps1
@@ -1,22 +1,49 @@
+# # Set the Maven version you want to install
+# $mavenVersion = "3.9.4"
+# $mavenUrl = "https://archive.apache.org/dist/maven/maven-3/$mavenVersion/binaries/apache-maven-$mavenVersion-bin.zip"
+
+# # Download the Maven zip file
+# $downloadPath = "$env:TEMP\apache-maven-$mavenVersion-bin.zip"
+# Invoke-WebRequest -Uri $mavenUrl -OutFile $downloadPath
+
+# # Specify the directory to extract Maven
+# $installDir = "C:\Tools\Maven"
+# Expand-Archive -Path $downloadPath -DestinationPath $installDir -Force
+
+# # Set Maven Environment Variables
+# $env:M2_HOME = "$installDir\apache-maven-$mavenVersion"
+# $env:Path = "$env:M2_HOME\bin;$env:Path"
+
+# # Optionally, permanently set environment variables (requires admin rights)
+# [System.Environment]::SetEnvironmentVariable("M2_HOME", "$installDir\apache-maven-$mavenVersion", [System.EnvironmentVariableTarget]::Machine)
+# [System.Environment]::SetEnvironmentVariable("Path", "$env:M2_HOME\bin;$env:Path", [System.EnvironmentVariableTarget]::Machine)
+
+# # Verify installation
+# mvn -version
+#------- above is working from offical one ------
+
 # Set the Maven version you want to install
-$mavenVersion = "3.9.4"
-$mavenUrl = "https://archive.apache.org/dist/maven/maven-3/$mavenVersion/binaries/apache-maven-$mavenVersion-bin.zip"
+$mavenVersion = "3.9.9"
+$mavenUrl = "https://prod.artifactory.nfcu.net:443/artifactory/cicd-generic-release-local/maven/$mavenVersion/windows/maven-$mavenVersion.zip"
 
 # Download the Maven zip file
-$downloadPath = "$env:TEMP\apache-maven-$mavenVersion-bin.zip"
+$downloadPath = "$env:TEMP\apache-maven-$mavenVersion.zip"
 Invoke-WebRequest -Uri $mavenUrl -OutFile $downloadPath
 
 # Specify the directory to extract Maven
-$installDir = "C:\Tools\Maven"
+$installDir = "C:\software\Maven"
 Expand-Archive -Path $downloadPath -DestinationPath $installDir -Force
 
 # Set Maven Environment Variables
-$env:M2_HOME = "$installDir\apache-maven-$mavenVersion"
+$env:M2_HOME = "$installDir\maven-$mavenVersion"
 $env:Path = "$env:M2_HOME\bin;$env:Path"
 
 # Optionally, permanently set environment variables (requires admin rights)
-[System.Environment]::SetEnvironmentVariable("M2_HOME", "$installDir\apache-maven-$mavenVersion", [System.EnvironmentVariableTarget]::Machine)
+[System.Environment]::SetEnvironmentVariable("M2_HOME", "$installDir\maven-$mavenVersion", [System.EnvironmentVariableTarget]::Machine)
 [System.Environment]::SetEnvironmentVariable("Path", "$env:M2_HOME\bin;$env:Path", [System.EnvironmentVariableTarget]::Machine)
 
-# Verify installation
-mvn -version
+# Refresh environment variables for the current session
+$env:Path = [System.Environment]::GetEnvironmentVariable("Path", [System.EnvironmentVariableTarget]::Machine)
+
+# Verify installation by calling mvn from the new path
+Start-Process "mvn" -ArgumentList "-version" -NoNewWindow -Wait
