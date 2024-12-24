0c7ec6262306ec5b2eba958e4103765d6cb128bf
Mohan0607 <76526200+Mohan0607@users.noreply.github.com>
2024-12-24 21:25:57 +0530

    Create ant.ps1

---
+++ scriptss-ps/renamed/ant.ps1
@@ -0,0 +1,51 @@
+# Fetch Ant tool details from the manifest
+$antTool = Get-ManifestTool -Name "Ant"
+$installArgs = $($antTool.installArgs, "/DIR=$($antTool.installPath)")
+
+# Verify the tool's source is from Artifactory
+if ($antTool.source -ne "artifactory") {
+    throw "Unable to install Ant. The specified source, '$($antTool.source)', is not supported."
+}
+
+# Ensure the tool exists in the manifest
+if ($null -eq $antTool) {
+    throw "Failed to get the tool 'Ant' from the manifest file. Verify the tool exists in the manifest or check the logs for additional error messages."
+}
+
+# Dynamically construct the Artifactory URL for the specific version of Ant
+$antVersion = $antTool.defaultVersion
+$url = "https://prod.artifactory.nfcu.net:443/artifactory/cicd-build-agent-local/apache-ant/windows/apache-ant-$($antVersion).zip"
+
+# Define the installation path and extracted directory
+$antPath = "C:\apache-ant"
+$antExtractedPath = "$antPath\apache-ant-$($antVersion)"
+
+# Use Install-Binary to handle the download and extraction
+Write-Host "Installing Ant version $antVersion from $url"
+Install-Binary `
+    -Url $url `
+    -Type Zip `
+    -DestinationPath $antPath `
+    -ErrorAction Stop
+
+# Validate the installation directory
+if (-Not (Test-Path "$antExtractedPath\bin\ant.bat")) {
+    throw "Ant installation failed. The required binary file is missing in the installation directory."
+}
+
+# Set environment variables for Ant
+Write-Host "Setting ANT_HOME and updating PATH environment variable"
+[Environment]::SetEnvironmentVariable('ANT_HOME', $antExtractedPath, [EnvironmentVariableTarget]::Machine)
+$currentPath = [System.Environment]::GetEnvironmentVariable("Path", [System.EnvironmentVariableTarget]::Machine)
+if ($currentPath -notlike "*$antExtractedPath\bin*") {
+    [System.Environment]::SetEnvironmentVariable("Path", "$currentPath;$antExtractedPath\bin", [System.EnvironmentVariableTarget]::Machine)
+}
+
+# Verify Ant installation
+Write-Host "Verifying Ant installation..."
+try {
+    $antVersionOutput = & "$antExtractedPath\bin\ant.bat" -version
+    Write-Host "Apache Ant installed successfully: $antVersionOutput"
+} catch {
+    throw "Failed to verify Apache Ant installation. Error: $_"
+}
